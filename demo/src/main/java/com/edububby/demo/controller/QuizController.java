a47de55781cdbc09105b910018e9a8257c14af6e
신충용 <cystudy93@gmail.com>
2024-12-09 16:54:05 +0900

    커밋

--- demo/src/main/java/com/edububby/demo/controller/QuizController.java
+++ demo/src/main/java/com/edububby/demo/controller/QuizController.java
@@ -3,12 +3,6 @@ package com.edububby.demo.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-<<<<<<< HEAD
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-=======
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -16,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> 09c278fc7fada32d93f6adf70e9a6835360c4c5b
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
